6c865b60b9edd5f5c37dd8ab7087d2ecf2f58cf9
Refa3e <ahmed.elshikh550@gmail.com>
2024-07-02 02:12:53 +0300

    Add project files.

---
+++ ContactsManagerTests/CountriesServiceTest.cs
@@ -0,0 +1,68 @@
+using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+
+namespace ContactsManagerTests
+{
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService _countriesService;
+        public CountriesServiceTest()
+        {
+            _countriesService = new CountriesService();
+        }
+
+
+        //When the CountryName is null, it should throw ArgumentException
+        [Fact]
+        public void AddCountry_NullCountryName()
+        {
+            //Arrange
+            CountryAddRequest request = new()
+            {
+                CountryName = null
+            };
+
+            //Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                //Act
+                _countriesService.AddCountry(request);
+            });
+        }
+
+        //When the CountryName is duplicate, it should throw ArgumentException
+        [Fact]
+        public void AddCountry_DuplicateCountryName()
+        {
+            //Arrange
+            CountryAddRequest request1 = new() { CountryName = "Egypt" };
+            CountryAddRequest request2 = new() { CountryName = "Egypt" };
+
+            //Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                //Act
+                _countriesService.AddCountry(request1);
+                _countriesService.AddCountry(request2);
+            });
+        }
+
+        //When you supply proper country name, it should insert (add) the country to the existing list of countries
+        [Fact]
+        public void AddCountry_ProperCountryName()
+        {
+            //Arrange
+            CountryAddRequest request = new() { CountryName = "Egypt" };
+
+            //Act
+            CountryResponse response = _countriesService.AddCountry(request);
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.NotEqual(Guid.Empty, response.CountryID);
+            Assert.Equal("Egypt", response.CountryName);
+        }
+
+    }
+}
---
+++ ServiceContracts/DTO/CountryAddRequest.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+
+namespace ServiceContracts.DTO
+{
+    public class CountryAddRequest
+    {
+        public string? CountryName { get; set; }
+
+        public Country ToCountry()
+        {
+           return new Country()
+           { CountryName = CountryName};
+        }
+    }
+}
